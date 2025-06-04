fc74eaa97f3771848fbc4595b66ea79943c25ac2
Crisapps99 <158056254+Crisapps99@users.noreply.github.com>
2025-06-04 10:53:48 -0500

    comiteado mi loco

--- assets/App/screens/Login.tsx
+++ assets/App/screens/Login.tsx
@@ -37,7 +37,7 @@ const Login = () => {
                 <TextInput style={styles.input} placeholder='Contraseñasss'
                     secureTextEntry={true}
                     onChangeText={(text: string)=> setPassword(text)} value={password}/>
-                <Button onPress={login} title='Iniciar Sesión'/>
+                <Button onPress={login} title='Iniciar Sesión en esta maldita cuenta mi loco '/>
                 <Button onPress={register} title="Crear una cuenta"/>
             </View>
         </View>
